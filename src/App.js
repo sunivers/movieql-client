fc2c61e841270c8ad66f1aca5a49090ad93c8082
soyoung <soyoungp02@gmail.com>
2020-01-19 21:14:45 +0900

    styling movie list

--- src/App.js
+++ src/App.js
@@ -1,17 +1,24 @@
 import React from 'react';
 import { HashRouter as Router, Route } from 'react-router-dom';
 import { ApolloProvider } from 'react-apollo';
+import styled from 'styled-components';
 import client from './apolloClient';
 import Home from './Home';
 import Detail from './Detail';
 
+const RouterWrapper = styled.div`
+  text-align: center;
+`
+
 function App() {
   return (
     <ApolloProvider client={ client }>
-      <Router>
-        <Route exact={true} path={"/"} component={Home}></Route>
-        <Route path={"/details/:movieId"} component={Detail}></Route>
-      </Router>
+      <RouterWrapper>
+        <Router>
+          <Route exact={true} path={"/"} component={Home}></Route>
+          <Route path={"/details/:movieId"} component={Detail}></Route>
+        </Router>
+      </RouterWrapper>
     </ApolloProvider>
   );
 }
--- src/Home.js
+++ src/Home.js
@@ -1,15 +1,42 @@
 import React from 'react';
 import { Query } from 'react-apollo';
 import { HOME_PAGE } from './queries';
+import styled from 'styled-components';
+
+const Card = styled.div`
+  display: inline-block;
+  vertical-align: top;
+  width: 200px;
+  height: 300px;
+  margin: 0 50px 20px 0;
+  border-radius: 15px;
+  box-shadow: 2px 3px 5px 2px #aaa;
+  background-image: url(${props => props.image});
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-position: center;
+`
+
+const Title = styled.h1`
+  background-color: rgba(0,0,0,0.6);
+  color: white;
+  font-size: 16px;
+  font-weight: 400;
+  margin-top: 200px;
+  text-align: center;
+`
 
 const Home = () => <Query query={HOME_PAGE}>
   {({ loading, data, error}) => {
     if(loading) return 'loading';
     if(error) return 'error';
     return data.movies.map(movie => (
-      <h2 key={movie.id}>
-        {movie.title} / {movie.rating}
-      </h2>
+      <Card key={movie.id} image={movie.medium_cover_image}>
+        <Title>
+          {movie.title} <br></br>
+          {movie.rating} ⭐ 
+        </Title>
+      </Card>
     ))
   }}
 </Query>;
